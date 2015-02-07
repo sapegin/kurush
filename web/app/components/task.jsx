42799b6224db76397e684f11e2ccc1f7d0a3138e
Artem Sapegin <artem@sapegin.ru>
2015-02-07 15:48:22 +0300

    Edit task.

--- web/app/components/task.jsx
+++ web/app/components/task.jsx
@@ -7,10 +7,50 @@ define(function(require, exports, module) {
 	 * @jsx React.DOM
 	 */
 	module.exports = React.createExtendedClass({
+		getInitialState: function() {
+			return {
+				edit: false
+			};
+		},
+
+		toggleEditMode: function() {
+			this.setState({edit: !this.state.edit});
+		},
+
+		save: function(event) {
+			event.preventDefault();
+			var values = this.getFormData(event.target);
+			this.model().set(values);
+			this.toggleEditMode();
+		},
+
 		render: function() {
+			if (this.state.edit) {
+				return this.renderEdit();
+			}
+			else {
+				return this.renderView();
+			}
+		},
+
+		renderView: function() {
 			return (
 				<li>
-					[{this.get('name')}, {this.get('project')}, {this.get('client')}]
+					{this.get('name')}, {this.get('project')}, {this.get('client')}
+					<button onClick={this.toggleEditMode}>Edit</button>
+				</li>
+			);
+		},
+
+		renderEdit: function() {
+			return (
+				<li>
+					<form onSubmit={this.save}>
+						<input type="text" defaultValue={this.get('name')} ref="name"/>
+						<input type="text" defaultValue={this.get('project')} ref="project"/>
+						<input type="text" defaultValue={this.get('client')} ref="client"/>
+						<input type="submit" value="Save"/>
+					</form>
 				</li>
 			);
 		}
--- web/app/util/react-extras.js
+++ web/app/util/react-extras.js
@@ -1,11 +1,30 @@
 define(function(require, exports, module) {
 	'use strict';
 
+	var _ = require('underscore');
 	var React = require('react');
 
-	var BackboneViewMixin = {
+	var slice = Array.prototype.slice;
+
+	React.KurushMixin = {
 		get: function(field) {
 			return this.props.model.get(field);
+		},
+
+		getFormData: function(form) {
+			var ReactNodeIdToRefName = {};
+			for (var refName in this.refs) {
+				ReactNodeIdToRefName[this.refs[refName]._rootNodeID] = refName;
+			}
+
+			var fields = slice.call(form.elements);
+			var values = {};
+			fields.forEach(function(field) {
+				var refName = ReactNodeIdToRefName[field.getAttribute('data-reactid')];
+				if (!refName) return;
+				values[refName] = this.refs[refName].getDOMNode().value
+			}.bind(this));
+			return values;
 		}
 	};
 
@@ -13,7 +32,7 @@ define(function(require, exports, module) {
 		var currentMixins = spec.mixins || [];
 
 		spec.mixins = currentMixins.concat([
-			BackboneViewMixin
+			React.KurushMixin
 		]);
 
 		return React.createBackboneClass(spec);
